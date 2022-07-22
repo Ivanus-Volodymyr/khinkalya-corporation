9bc1d85de5ad3d5174f1c9ff66088cbbfb547e18
ivanys23071996 <ivanys230796@gmail.com>
2022-07-22 22:51:32 +0300

    add user update

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Put,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
 import { UserService } from "./user.service";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { User } from "@prisma/client";
 
 @Controller("user")
 export class UserController {

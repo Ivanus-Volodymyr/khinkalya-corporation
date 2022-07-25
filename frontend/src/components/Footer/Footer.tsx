fe50f5a0a3ab99dfb00694e1a7787e699104d1fa
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-07-25 16:38:55 +0300

    solve problems and add gitignore files

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -2,8 +2,7 @@ import React, {FC} from 'react';
 import './Footer.css'
 const Footer :FC= () => {
     return (
-        <div >
-
+        <div>
             <div className={'footer'}>
                 <hr/>
                 <a href="/"> <img src="/image-for-header/logoKhinkalnya.jpg" alt=""/></a>

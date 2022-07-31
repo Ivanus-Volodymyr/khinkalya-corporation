2a51d2f717dd8e3174685bf583b96d76fbacb147
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-07-31 20:10:23 +0300

    fix conflicts

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,4 +1,6 @@
 import { Module } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 import { PrismaService } from "../core/prisma.service";
@@ -7,6 +9,6 @@ import { S3Service } from "../s3/s3.service";
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [UserService, PrismaService, S3Service],
+  providers: [UserService, PrismaService, S3Service, JwtService],
 })
 export class UserModule {}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,27 +12,26 @@ import {
       UserRegistration,
       Users,
       Cart,
+      DishList,
     } from './components';
 import OrderDone from "./components/OrderDone/OrderDone";
 
 function App() {
 
     return (
-        <div>
-            <Routes>
-                <Route path={"/"} element={<Layout/>}>
-                    <Route path={'/main'} element={<Main/>}></Route>
-                    <Route path={'/auth/registration'} element={<UserRegistration/>}></Route>
-                    <Route path={'/auth/login'} element={<UserLogin/>}></Route>
-                    <Route path={'/admin/AddDish'} element={<AddDish/>}></Route>
-                    <Route path={'/admin'} element={<Admin/>}></Route>
-                    <Route path={'/users'} element={<Users/>}></Route>
-                    <Route path={'/dish/:id'} element={<DishList/>}></Route>
-                    <Route path={'/cart'} element={<Cart/>}></Route>
-                    <Route path={'/cart/orderDone'} element={<OrderDone/>}></Route>
-                </Route>
-            </Routes>
-        </div>
+      <Routes>
+        <Route path={"/"} element={<Layout/>}>
+          <Route path={'/main'} element={<Main/>}></Route>
+          <Route path={'/auth/registration'} element={<UserRegistration/>}></Route>
+          <Route path={'/auth/login'} element={<UserLogin/>}></Route>
+          <Route path={'/admin/AddDish'} element={<AddDish/>}></Route>
+          <Route path={'/admin'} element={<Admin/>}></Route>
+          <Route path={'/users'} element={<Users/>}></Route>
+          <Route path={'/dish/:id'} element={<DishList/>}></Route>
+          <Route path={'/cart'} element={<Cart/>}></Route>
+          <Route path={'/cart/orderDone'} element={<OrderDone/>}></Route>
+        </Route>
+      </Routes>
     );
 }
 

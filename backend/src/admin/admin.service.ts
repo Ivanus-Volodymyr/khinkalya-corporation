bc0c3f705791bd1f3b1dcbf0ddb69987f815e352
ivanys23071996 <ivanys230796@gmail.com>
2022-08-03 22:11:25 +0300

    start adding admin functionality

--- backend/src/admin/admin.service.ts
+++ backend/src/admin/admin.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from "@nestjs/common";
+import { Dish } from "@prisma/client";
+
 import { PrismaService } from "../core/prisma.service";
+import { LocalityService } from "../locality/locality.service";
+import { RestaurantService } from "../restaurant/restaurant.service";
+import { DishService } from "../dish/dish.service";
 
 @Injectable()
 export class AdminService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private localityService: LocalityService,
+    private restaurantService: RestaurantService,
+    private dishService: DishService
+  ) {}
+
+  public async addDish(dish: Dish, file): Promise<Dish> {
+    return this.dishService.createDish(dish, file);
+  }
 }

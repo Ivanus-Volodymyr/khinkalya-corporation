5506508ed8a7da432a7e977c9255d4051da1952a
AndrewMilovich <triplle@ukr.net>
2022-08-05 16:43:31 +0300

    add Loader

--- frontend/src/components/Loader/Loader.tsx
+++ frontend/src/components/Loader/Loader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import './Loader.css'
 const Loader = () => {
   return (
-    <div  style={{display:'flex',flexDirection:"column",alignItems:"center"}} >
+    <div  style={{display:'flex',flexDirection:"column",alignItems: "center"}} >
       <div  className="lds-hourglass"></div>
       </div>
   );

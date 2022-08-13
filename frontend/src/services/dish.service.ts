20ddf9531a9f9343dccb7a6d34ef872d47aa1261
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-08-14 00:24:39 +0300

    Revert "Revert "Feature/30 popup frequent order ""

--- frontend/src/services/dish.service.ts
+++ frontend/src/services/dish.service.ts
@@ -1,7 +1,9 @@
-import { IDish } from '../interfaces/dish.interface';
+import { IDish } from '../interfaces';
 import { axiosService } from './axios.service';
 import { urls } from '../constants';
 
 export const dishService = {
-  getDish: () => axiosService.get<IDish[]>(urls.dish),
+  getAllDishes: () => axiosService.get<IDish[]>(urls.dish),
+  getDishById: (dishId: number) =>
+    axiosService.get<IDish>(urls.popularDish + `/${dishId}`),
 };

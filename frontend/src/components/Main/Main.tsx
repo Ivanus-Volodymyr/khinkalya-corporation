451818b923c9966c19843b72cbdf0ee395d1eb4a
AndrewMilovich <triplle@ukr.net>
2022-08-15 20:36:09 +0300

    change logo

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,15 +1,13 @@
 import React, { FC } from "react";
 
 const Main: FC = () => {
+
   return (
-    <div
-      style={{ margin: 0, height: "initial", position: "fixed", zIndex: -1 }}
-    >
-      <video
+    <div>
+      <video style={{ margin: 0, height: "initial", position: "fixed", zIndex: -1 }}
         width={"100%"}
         autoPlay
         muted
-        loop={true}
         className="video"
         src={
           "/video/video.mp4"

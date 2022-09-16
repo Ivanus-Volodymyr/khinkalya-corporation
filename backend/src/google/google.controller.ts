18ae49730c4bffd05cdf7e958010daa8c2f1f16d
ivanys23071996 <ivanys230796@gmail.com>
2022-09-16 17:09:04 +0300

    google controller

--- backend/src/google/google.controller.ts
+++ backend/src/google/google.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query } from "@nestjs/common";
 
 import { LoginGoogleDto } from "./dto/google.dto";
 import { GoogleService } from "./google.service";
